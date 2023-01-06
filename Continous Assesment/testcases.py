b5c1d6a613b3bf16388683c22ab53a83f74a6b07
Aniket-Nimbalkar <102214567+Aniket-Nimbalkar@users.noreply.github.com>
2023-01-06 12:55:13 +0530

    Update testcases.py

--- Continous Assesment/testcases.py
+++ Continous Assesment/testcases.py
@@ -1,60 +1,28 @@
-import random
-import smtplib
-# import unittest
-from email_validator import validate_email
+import unittest
+import otp_v1 as o
 
-# smptlib Gmail PORT
-PORT = 587
+class TestOtp(unittest.TestCase):
+    def test_email(self):
+        self.assertIn("@",o.userEmail,"Invalid Email")
+        self.assertIn(".com",o.userEmail, "Invalid Email")
+    def test_otpLength(self):
+        otp = o.generateOtp
+        self.assertGreaterEqual(len(otp),6,"Length of otp should be greater or equal to 6")
 
-# Importing my Email and Password
-file = open("confidential.txt", "r")
-myEmail = ""
-myPass = ""
-for data in file:
-    value = data.split(",")
-    myEmail = value[0]
-    myPass = value[1]
+if __name__ == '__main__':
+   unittest.main()
 
-# Input of User Email Id
-userEmail = input("Enter you Email id : ")
+=======
+import unittest
+import otp_v1 as o
 
-# Text message to be send
-message = "Otp to login is "
+class TestOtp(unittest.TestCase):
+    def test_email(self):
+        self.assertIn("@",o.userEmail,"Invalid Email")
+        self.assertIn(".com",o.userEmail, "Invalid Email")
+    def test_otpLength(self):
+        otp = o.generateOtp
+        self.assertGreaterEqual(len(otp),6,"Length of otp should be greater or equal to 6")
 
-try:
-    # Validating the Email
-    validated_email = validate_email(userEmail)
-except:
-    print("Invalid Email");
-
-
-# Function for sending email to user
-def sendEmailtoUser():
-    server.sendmail(myEmail , userEmail , message + str(otp) )
-
-    # Quiting the server after sending email
-    server.quit();
-
-def generateOtp():
-    otp = random.randrange(1000,10000)
-    return otp
-
-try:
-    server = smtplib.SMTP('smtp.gmail.com', PORT)
-    server.starttls()
-    server.login(myEmail,myPass)
-    otp = generateOtp()
-    
-    # sending Email to User
-    sendEmailtoUser()
-
-    #Input from user to validate OTP
-    isCorrectOtp = int(input(("Enter your Otp : ")))
-
-    # Validating entered Otp
-    if(isCorrectOtp == otp):
-        print("Logged in Successfully!")
-    else:
-        print("Invalid Otp!")
-except:
-    print("An Exception occured!")
+if __name__ == '__main__':
+   unittest.main()
